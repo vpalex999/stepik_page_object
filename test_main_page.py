04a0584ec21085a565b8fd3f21a16d8ec6ca72c8
vpalex999 <vpalex@mail.ru>
2022-02-05 15:31:48 +0500

    go to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,7 @@
 
+from pages.main_page import MainPage
+
+
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
@@ -6,12 +9,13 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    browser.get(link)
-    go_to_login_page(browser)
-
-def test_add_to_cart(browser):
-    page = ProductPage(url="", browser)
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_add_to_catr_button()
-    page.app_product_to_cart()
-    page.should_be_success_message()
+    page.go_to_login_page()
+
+# def test_add_to_cart(browser):
+#     page = ProductPage(url="", browser)
+#     page.open()
+#     page.should_be_add_to_catr_button()
+#     page.app_product_to_cart()
+#     page.should_be_success_message()
