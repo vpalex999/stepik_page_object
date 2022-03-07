49fa5ef410ead3c6b557a26d9a2d56bbdb69d446
vpalex999 <vpalex@mail.ru>
2022-03-07 16:31:01 +0500

    add Test class

--- test_main_page.py
+++ test_main_page.py
@@ -3,25 +3,28 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 
+
 @pytest.fixture
 def link_main():
     return "http://selenium1py.pythonanywhere.com"
 
 
-def test_guest_can_go_to_login_page(browser, link_main):
-    link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link_main)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
 
+    def test_guest_can_go_to_login_page(self, browser, link_main):
+        link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        page = MainPage(browser, link_main)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_quest_should_see_login_link(browser, link_main):
-    link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link_main)
-    page.open()
-    page.should_be_login_link()
+    def test_quest_should_see_login_link(self, browser, link_main):
+        link2 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        page = MainPage(browser, link_main)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, link_main):
